9292d13e36e3260bc0b5b97249d2106cf648a8fe
Hare Prananda <hare.prananda@odeo.co.id>
2022-03-03 18:59:28 +0800

    Configure eslint and prettier

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const ESLintPlugin = require("eslint-webpack-plugin")
+const ESLintOption = require("./.eslintrc.json")
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.plugins.push(new ESLintPlugin({ overrideConfig: ESLintOption }))
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
